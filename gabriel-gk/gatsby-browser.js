9131f794334c07f86389f9bb8364ee1a16749e54
Gabriel_Silva <gabriel.ieda@hotmail.com>
2021-12-29 17:35:26 -0300

    -Ajuste de Animação ao retorna a pagina anterior

--- gabriel-gk/gatsby-browser.js
+++ gabriel-gk/gatsby-browser.js
@@ -1,5 +1,25 @@
 import React from 'react';
 import { AnimatePresence } from 'framer-motion';
+
 export const wrapPageElement = ({ element }) => (
   <AnimatePresence exitBeforeEnter>{element}</AnimatePresence>
 );
+
+export const shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition
+}) => {
+  // transition duration from `layout.js` * 1000 to get time in ms
+  // * 2 for exit + enter animation
+  const TRANSITION_DELAY = 0.3 * 1000 * 2
+  // if it's a "normal" route
+  if (location.action === "PUSH") {
+    window.setTimeout(() => window.scrollTo(0, 0), TRANSITION_DELAY)
+  }
+  // if we used the browser's forwards or back button
+  else {
+    const savedPosition = getSavedScrollPosition(location) || [0, 0]
+    window.setTimeout(() => window.scrollTo(...savedPosition), TRANSITION_DELAY)
+  }
+  return false
+}
